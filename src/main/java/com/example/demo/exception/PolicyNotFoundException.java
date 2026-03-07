364385ef4790f45c1034ba709f2331fc6356d621
cse-aditya-manthina <cse.aditya.seshasai.manthina@gmail.com>
2026-03-07 23:11:08 +0530

    Cleared repository

--- src/main/java/com/example/demo/exception/PolicyNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class PolicyNotFoundException extends RuntimeException {
-
-    public PolicyNotFoundException(String message) {
-        super(message);
-    }
-}
